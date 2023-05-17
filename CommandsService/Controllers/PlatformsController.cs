3886c79b7d634dcb285d1a453af0bcde5448b364
omerunsal <omerunsal94@outlook.com>
2023-05-17 17:53:20 +0300

    inbound test

---
+++ CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers
+{
+    [Route("api/c/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        public PlatformsController()
+        {
+
+        }
+
+        public ActionResult TestInboundConnection()
+        {
+            System.Console.WriteLine("TEST POST from Command Service");
+            return Ok("Inbound test of from Platforms Controller");
+        }
+    }
+}
